936d8b2d052fbd07cc71a028d997ea8a16b5a351
Brian Ewing <me@brianewing.co.uk>
2013-02-02 15:08:48 +0000

    simply/refactor script

--- src/bytes.js
+++ src/bytes.js
@@ -1,23 +1,19 @@
 // (c) 2013 Brian J Ewing
 // MIT
 
-function init() {
-  chrome.experimental.processes.onUpdated.addListener(function(processes) {
-    // alert('aye');
-    for(var pid in processes) {
-      var process = processes[pid];
-      if(process.type != 'renderer') continue;
+chrome.experimental.processes.onUpdated.addListener(function(processes) {
+  for(var pid in processes) {
+    var process = processes[pid];
+    if(process.type != 'renderer') continue;
 
-      var title = process.network > 1024 ? '' + Math.round(process.network / 1024) + 'kB/s' : '' + process.network + 'B/s';
+    var title = process.network > 1024 ? '' + Math.round(process.network / 1024) + 'kB/s' : '' + process.network + 'B/s';
+    var script = "var originalTitle = document.title.replace(/^\\[\\d+k?B\\/s\\] /, '');";
 
-      if(process.network > 0)
-        var script = "var originalTitle = document.title.replace(/^\\[\\d+k?B\\/s\\] /, ''); document.title = '[" + title + "] ' + originalTitle;";
-      else
-        var script = "var originalTitle = document.title.replace(/^\\[\\d+k?B\\/s\\] /, ''); document.title = originalTitle || document.title;";
+    if(process.network > 0)
+      script += "document.title = '[" + title + "] ' + originalTitle;";
+    else
+      script += "document.title = originalTitle;";
 
-      chrome.tabs.executeScript(process.tabs[0], {code: script});
-    };
-  });
-}
-
-init();
+    chrome.tabs.executeScript(process.tabs[0], {code: script});
+  };
+});

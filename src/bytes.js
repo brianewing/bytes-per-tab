93a94fc9664459977e80c011d0cd2ea3d804d369
Brian Ewing <me@brianewing.co.uk>
2013-02-02 14:41:46 +0000

    vastly improve the title replacement by regex-replacing our addition instead of caching the original title

--- src/bytes.js
+++ src/bytes.js
@@ -13,9 +13,9 @@ function init() {
       var title = process.network > 1024 ? '' + Math.round(process.network / 1024) + 'kB/s' : '' + process.network + 'B/s';
 
       if(process.network > 0)
-        var script = "window._lastTitle = (window._lastTitle || document.title); document.title = '[" + title + "] ' + _lastTitle;";
+        var script = "var originalTitle = document.title.replace(/^\\[\\d+k?B\\/s\\] /, ''); document.title = '[" + title + "] ' + originalTitle;";
       else
-        var script = "document.title = window._lastTitle || document.title;";
+        var script = "var originalTitle = document.title.replace(/^\\[\\d+k?B\\/s\\] /, ''); document.title = originalTitle || document.title;";
 
       chrome.tabs.executeScript(process.tabs[0], {code: script});
     };
